848cc6ac1c73d028395acb426a579fc7237dce29
Thaila <86130523+smlltox@users.noreply.github.com>
2023-02-24 00:31:53 -0300

    Add files via upload

--- Site/script.js
+++ Site/script.js
@@ -1,15 +1,3 @@
-const loginScreen = document.querySelector('.login-screen');
-      
-window.addEventListener('load', () => {
-  loginScreen.style.opacity = '0';
-  loginScreen.style.transform = 'translateY(-100px)';
-
-  setTimeout(() => {
-    loginScreen.style.opacity = '1';
-    loginScreen.style.transform = 'translateY(0)';
-  }, 500);
-});
-
 function mostrarLista() {
 	var lista = document.getElementById("minhaLista");
 	if (lista.style.display === "none") {
@@ -19,3 +7,16 @@ function mostrarLista() {
 	}
 }
 
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    var objeto = document.getElementById('inicial');
+    objeto.style.transform = "translateY(500px)";
+  }, 500); // atraso de 500ms
+});
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    var objeto = document.querySelector('.login-screen');
+    objeto.style.transform = "translateY(700px)";
+  }, 500); // atraso de 500ms
+});
