be7d08cbf4dbb5b75dc0b883957af28c9fbea3d8
Thaila <86130523+smlltox@users.noreply.github.com>
2023-02-24 06:05:51 -0300

    Add files via upload

--- Site/script.js
+++ Site/script.js
@@ -1,3 +1,5 @@
+//mostra porfolio de cada tatuador
+
 function mostrarLista() {
 	var lista = document.getElementById("minhaLista");
 	if (lista.style.display === "none") {
@@ -7,12 +9,16 @@ function mostrarLista() {
 	}
 }
 
+//tela inicial
+
 window.addEventListener('load', function() {
   setTimeout(function() {
     var objeto = document.getElementById('inicial');
-    objeto.style.transform = "translateY(500px)";
+    objeto.style.transform = "translateY(-500px)";
   }, 500); // atraso de 500ms
 });
+
+//telinhas de login/cadastro/orçamentos
 
 window.addEventListener('load', function() {
   setTimeout(function() {
@@ -20,3 +26,24 @@ window.addEventListener('load', function() {
     objeto.style.transform = "translateY(700px)";
   }, 500); // atraso de 500ms
 });
+
+//imagens em destque no index
+
+var imagens = document.querySelectorAll('#galeria img');
+var meio = Math.floor(imagens.length / 2); // índice da imagem do meio
+
+for (var i = 0; i < imagens.length; i++) {
+  if (i === meio) {
+    imagens[i].classList.add('grande');
+  } else {
+    imagens[i].classList.add('pequena');
+  }
+
+  imagens[i].addEventListener('mouseover', function() {
+    this.classList.add('grande');
+  });
+
+  imagens[i].addEventListener('mouseout', function() {
+    this.classList.remove('grande');
+  });
+}
